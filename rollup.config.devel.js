57212d6dd5215efd465657527915fe3952b42f9c
Matthew Wardrop <mawardrop@netflix.com>
2018-10-20 14:37:44 -0700

    Allow importing of commonjs modules.

--- rollup.config.devel.js
+++ rollup.config.devel.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 
@@ -13,6 +14,9 @@ export default {
     ],
     plugins: [
         resolve(),
+        commonjs({
+            exclude: ['src/**']
+        }),
         babel({
             exclude: 'node_modules/**'
         }),
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import {uglify} from "rollup-plugin-uglify";
 import postcss from 'rollup-plugin-postcss';
@@ -14,6 +15,9 @@ export default {
     ],
     plugins: [
         resolve(),
+        commonjs({
+            exclude: ['src/**']
+        }),
         babel({
             exclude: 'node_modules/**'
         }),
