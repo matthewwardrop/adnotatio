3bf039f000793025093fab7072e3f36775f1c2b5
Matthew Wardrop <mpwardrop@gmail.com>
2021-01-06 10:43:32 -0800

    Adjust linting configuration.

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,7 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 
 const dependencies = Object.keys(require('./package.json').dependencies);
 
